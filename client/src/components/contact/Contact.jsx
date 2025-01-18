de26b95b83a2891cb6b12286cd67d4449047fa1e
Radoslav <todorovradoslav6@gmail.com>
2025-01-18 13:15:08 +0200

    Make form and error handling

--- client/src/components/contact/Contact.jsx
+++ client/src/components/contact/Contact.jsx
@@ -1,48 +1,97 @@
 import { useState } from "react";
+import { requsterApi } from "../../service/requester";
 
-export default function Contact(){
-  const [data,setData] = useState({
-    name:"",
-    email:"",
-    message:""
-  })
+export default function Contact() {
+  const [data, setData] = useState({
+    name: "",
+    email: "",
+    message: "",
+  });
 
 
-  const onChangeHandler = (e) =>{
+  const [error, setError] = useState(false);
+  const [successModal,setSuccefulModal] = useState(false);
+
+  const onChangeHandler = (e) => {
     const name = e.target.name;
     const value = e.target.value;
 
-
     setData((oldData) => ({
       ...oldData,
-      [name]:value
-    }))
-  }
+      [name]: value,
+    }));
+  };
 
-  const onSubmitHandler = (e) =>{
+  const onSubmitHandler = async (e) => {
     e.preventDefault();
-    
-  }
+    try {
+      const res = await requsterApi.post(data);
+      if(res){
+        setSuccefulModal(true);
+        e.target.reset();
+      }
 
-    return(
+
+      setTimeout(() => {
+        setSuccefulModal(false);
+      },4000)
+        
+    } catch (err) {
+      setError(true)
+    } 
+  };
+
+  return (
     <>
-        <section id="contact" className="contact-section">
-  <h2>Contact</h2>
-  <div className="group">
-    <div className="text">
-Have a question or want to work together? Fill out the form below, and I’ll get back to you as soon as possible!
+    {successModal ? 
+    <div className="success-message"> 
+    <i className="fas fa-check-circle"></i>
+    <h1>Success!</h1> <p>You succefully send a message</p></div>
+    :""}
+    {error ?  <div className="error-message">
+    <span className="error-icon">⚠️</span>
+    Your message could not be sent. Please try again later!
+</div>:""}
+   <section id="contact" className="contact-section">
+    <h2>Contact</h2>
+    <div className="group">
+      <div className="text">
+        I'd love to hear from you!
+        Whether you have a question, a collaboration idea, feel free to drop me a message.
+        You can reach out to me directly with email at todorovradoslav18@gmail.com.
+        Looking forward to connecting with you! 
+      </div>
+      <form onSubmit={onSubmitHandler}>
+        <label htmlFor="name">Name</label>
+        <input
+          type="text"
+          id="name"
+          onChange={onChangeHandler}
+          name="name"
+          required
+        />
+        <label htmlFor="email">Email</label>
+        <input
+          type="text"
+          name="email"
+          id="email"
+          onChange={onChangeHandler}
+          required
+        />
+        <label htmlFor="message">Message</label>
+        <textarea
+          id="message"
+          cols={30}
+          rows={10}
+          defaultValue={""}
+          name="message"
+          onChange={onChangeHandler}
+          required
+        />
+        <button type="submit">Send Message</button>
+      </form>
     </div>
-    <form onSubmit={onSubmitHandler}>
-      <label htmlFor="name">Name</label>
-      <input type="text" id="name" onChange={onChangeHandler} name="name"/>
-      <label htmlFor="email">Email</label>
-      <input type="text" name="email" id="email"  onChange={onChangeHandler} />
-      <label htmlFor="message">Message</label>
-      <textarea id="message" cols={30} rows={10} defaultValue={""} name="message"  onChange={onChangeHandler}/>
-      <button type="submit">Send Message</button>
-    </form>
-  </div>
-</section>
-
-    </>);
+  </section> 
+    </>
+  );
 }
